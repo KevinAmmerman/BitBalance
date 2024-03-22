77ba54a6551efd2ec71fb821f3e8c2dd0e246ab2
Kevin Ammerman <127098309+KevinAmmerman@users.noreply.github.com>
2024-03-22 11:47:54 +0100

    form for add transaction styled, validation and error handling implemented

--- src/app/add-transaction-dialog/add-transaction-dialog.component.ts
+++ src/app/add-transaction-dialog/add-transaction-dialog.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { NgbActiveModal, NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgbActiveModal, NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
 import { Transaction } from '../shared/modules/transaction';
 import { FirestoreDataService } from '../shared/services/firestore-data.service';
 
@@ -14,11 +14,11 @@ import { FirestoreDataService } from '../shared/services/firestore-data.service'
 export class AddTransactionDialogComponent {
 
   addTransactionForm: FormGroup;
-  return: any;
 
   constructor(public modal: NgbActiveModal, private firebaseService: FirestoreDataService) {
     this.addTransactionForm = new FormGroup({
-      bitcoin: new FormControl({ value: '', disabled: false }, Validators.required),
+      bitcoin: new FormControl({ value: '', disabled: false }, [Validators.required]),
+      unitType: new FormControl({ value: 'Unit', disabled: false }, Validators.required),
       cost: new FormControl({ value: '', disabled: false }, Validators.required),
       transactionType: new FormControl({ value: 'Buy / Sell', disabled: false }, [Validators.required, Validators.pattern('buy|sell')]),
       date: new FormControl({ value: '', disabled: false }, Validators.required),
@@ -27,20 +27,22 @@ export class AddTransactionDialogComponent {
   }
 
   onSubmit() {
-    try {
-      if (this.addTransactionForm.valid) {
-        const formData = this.setFormData();
-        this.firebaseService.addDoc('test', formData.id, formData)
-      }
-    } catch (error) {
-      console.log('Test')
-    }
+    // try {
+    //   if (this.addTransactionForm.valid) {
+    //     const formData = this.setFormData();
+    //     this.firebaseService.addDoc('test', formData.id, formData)
+    //   }
+    // } catch (error) {
+    //   console.log('Test')
+    // }
+    console.log(this.addTransactionForm)
   }
 
 
   setFormData(): Transaction {
     return {
       amount: this.addTransactionForm.value.bitcoin,
+      unit: this.addTransactionForm.value.unitType,
       cost: this.addTransactionForm.value.cost,
       date: this.addTransactionForm.value.date,
       id: new Date().getTime(),
@@ -53,6 +55,7 @@ export class AddTransactionDialogComponent {
   resetForm() {
     this.addTransactionForm.reset({
       bitcoin: '',
+      unitType: 'Unit',
       currency: '',
       transactionType: 'Buy / Sell',
       date: '',
--- src/app/shared/modules/transaction.ts
+++ src/app/shared/modules/transaction.ts
@@ -1,6 +1,7 @@
 export interface Transaction {
     id: number;
     amount: number;
+    unit: string;
     cost: number;
     date: number;
     exchange: string;

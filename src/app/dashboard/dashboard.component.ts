295274e78fdbf861b541abe85b981a5548be69d3
Kevin Ammerman <127098309+KevinAmmerman@users.noreply.github.com>
2024-03-26 09:13:49 +0100

    created status component for showing current stats of bitcoin stack and value

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { BitcoinChartComponent } from '../bitcoin-chart/bitcoin-chart.component';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AddTransactionDialogComponent } from '../add-transaction-dialog/add-transaction-dialog.component';
+import { StatusComponent } from '../status/status.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [BitcoinChartComponent],
+  imports: [BitcoinChartComponent, StatusComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })

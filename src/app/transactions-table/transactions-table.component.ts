00f92b1b3a705a726679a853269b80e9635bfaa6
Kevin Ammerman <127098309+KevinAmmerman@users.noreply.github.com>
2024-03-27 22:44:09 +0100

    table styled

--- src/app/transactions-table/transactions-table.component.ts
+++ src/app/transactions-table/transactions-table.component.ts
@@ -1,41 +1,48 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { NgbPaginationModule, NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
+import { Subscription, map, tap } from 'rxjs';
 import { Transaction } from '../shared/modules/transaction';
-import { Observable, map } from 'rxjs';
 import { FirestoreDataService } from '../shared/services/firestore-data.service';
-import { AsyncPipe, CommonModule } from '@angular/common';
 import { UtilityService } from '../shared/services/utility.service';
 
 @Component({
 	selector: 'app-transactions-table',
 	standalone: true,
-	imports: [NgbPaginationModule, NgbTypeaheadModule, AsyncPipe, CommonModule],
+	imports: [NgbPaginationModule, NgbTypeaheadModule, CommonModule, FormsModule],
 	templateUrl: './transactions-table.component.html',
 	styleUrl: './transactions-table.component.scss'
 })
 export class TransactionsTableComponent {
 
-	page = 1;
-	pageSize = 4;
-	transactionData$: Observable<any> = new Observable()
-	// collectionSize = TransactionData.length;
-	// countries: Country[];
+	page: number = 1;
+	pageSize: number = 4;
+	transactions: Transaction[] = [];
+	collectionSize: number = 0;
+
+	transactionData: Transaction[] = [];
+	transactionDataSub: Subscription = new Subscription();
 
 	constructor(private firestoreDataService: FirestoreDataService, private utilityService: UtilityService) {
-		// this.refreshCountries();
-		this.transactionData$ = this.firestoreDataService.getCollection('test').pipe(
+		this.transactionDataSub = this.firestoreDataService.getCollection('test').pipe(
 			map((data: any[]) => data.map((data: Transaction) => ({
 				...data,
 				date: this.utilityService.getDate(data)
-		})))
-		);
+			})
+			)),
+			tap((data: any) => this.transactionData = data),
+			tap((data: any) => this.collectionSize = data.length),
+			tap(() => this.refreshTransactions())
+			).subscribe();
 	}
 
-	// refreshCountries() {
-	// 	this.countries = COUNTRIES.map((country, i) => ({ id: i + 1, ...country })).slice(
-	// 		(this.page - 1) * this.pageSize,
-	// 		(this.page - 1) * this.pageSize + this.pageSize,
-	// 	);
-	// }
 
+
+	refreshTransactions() {
+		this.transactions = this.transactionData.map((transaction, i) => ({ newId: i + 1, ...transaction })).slice(
+			(this.page - 1) * this.pageSize,
+			(this.page - 1) * this.pageSize + this.pageSize,
+		);
+	}
 }

a171d5c2f69fe948ee98cc8b9d8e212d03fa1ae8
Kevin Ammerman <127098309+KevinAmmerman@users.noreply.github.com>
2024-03-29 15:09:57 +0100

    styled login component

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  constructor() {}
 
 }

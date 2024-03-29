41932937ec86746432f38c099c5cff706f670270
Kevin Ammerman <127098309+KevinAmmerman@users.noreply.github.com>
2024-03-29 18:35:45 +0100

    implemented visual formcontrol in register form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,8 @@ import { LoginComponent } from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'sign-up', pathMatch: 'full' },
-    { path: 'login', component: LoginComponent },
+    { path: '', redirectTo: 'sign-in', pathMatch: 'full' },
+    { path: 'sign-in', component: LoginComponent },
     { path: 'dashboard', component: DashboardComponent },
     { path: 'sign-up', component: RegisterComponent}
 ];
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../shared/services/auth.service';
+import { Router } from '@angular/router';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { UserInterface } from '../../shared/modules/user.interface';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+
+  newUserForm: FormGroup;
+
+  constructor(
+    private authService: AuthService,
+    private route: Router
+    ) {
+      this.newUserForm = new FormGroup({
+        username: new FormControl({ value: '', disabled: false }, [Validators.required]),
+        email: new FormControl({ value: '', disabled: false }, [Validators.required, Validators.email]),
+        password: new FormControl({ value: '', disabled: false }, [Validators.required])
+      })
+    }
+
+    onSubmit() {
+      const rawUserData = this.newUserForm.getRawValue()
+      const newUser: UserInterface = {username: rawUserData.username, email: rawUserData.email, password: rawUserData.password}
+      this.authService.register(newUser).subscribe(() => {
+        this.route.navigateByUrl('sign-in');
+      });
+    }
 
 }

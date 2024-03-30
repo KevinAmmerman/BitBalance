ccaef246456e1476d3100d279b90ab465348f7ff
Kevin Ammerman <127098309+KevinAmmerman@users.noreply.github.com>
2024-03-30 13:31:08 +0100

    #

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Inject, inject } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../shared/services/auth.service';
 import { Router } from '@angular/router';
 import { UserInterface } from '../../shared/modules/user.interface';
 import { Subscription } from 'rxjs';
+import { Auth, FacebookAuthProvider, GithubAuthProvider, GoogleAuthProvider, OAuthProvider, signInWithPopup } from '@angular/fire/auth';
 
 
 @Component({
@@ -17,20 +18,21 @@ export class LoginComponent {
 
   loginForm: FormGroup;
   authSub: Subscription = new Subscription();
+  auth = inject(Auth)
 
   constructor(
     private authService: AuthService,
     private route: Router
-    ) {
+  ) {
     this.loginForm = new FormGroup({
-      email: new FormControl ({ value: '', disabled: false }, [Validators.required, Validators.email]),
-      password: new FormControl ({ value: '', disabled: false }, [Validators.required])
+      email: new FormControl({ value: '', disabled: false }, [Validators.required, Validators.email]),
+      password: new FormControl({ value: '', disabled: false }, [Validators.required])
     })
   }
 
   onSubmit() {
     const rawUserData = this.loginForm.getRawValue()
-    const newUser: UserInterface = {username: rawUserData.username, email: rawUserData.email, password: rawUserData.password}
+    const newUser: UserInterface = { username: rawUserData.username, email: rawUserData.email, password: rawUserData.password }
     this.authSub = this.authService.login(newUser).subscribe(({
       next: () => this.route.navigateByUrl('dashboard'),
       error: (err) => console.log(err)
@@ -41,4 +43,9 @@ export class LoginComponent {
     this.authSub.unsubscribe();
   }
 
+  googleSsoLogin() {
+    signInWithPopup(this.auth, new GoogleAuthProvider())
+      .then(user => this.route.navigateByUrl('dashboard'))
+      .catch((err: Error) => console.log(err.message));
+  }
 }

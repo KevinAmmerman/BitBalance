387053fc3abfebe73f8ad0b15a6af55b169a0a0e
Kevin Ammerman <127098309+KevinAmmerman@users.noreply.github.com>
2024-04-21 09:48:54 +0200

    extended register function with send email verification

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,51 +1,74 @@
 import { Injectable, inject } from '@angular/core';
-import { Auth, GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from '@angular/fire/auth';
+import {
+  Auth,
+  GoogleAuthProvider,
+  UserCredential,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  sendEmailVerification,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from '@angular/fire/auth';
 import { Router } from '@angular/router';
-import { BehaviorSubject, Observable, from } from 'rxjs';
+import { BehaviorSubject, Observable, catchError, from, map } from 'rxjs';
 import { UserInterface } from '../modules/user.interface';
+import { NotificationHandlingService } from './notification-handling.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  firebaseAuth = inject(Auth)
-  router = inject(Router)
+  firebaseAuth = inject(Auth);
+  router = inject(Router);
   userData: any;
-  user$: BehaviorSubject<any> = new BehaviorSubject('') 
+  user$: BehaviorSubject<any> = new BehaviorSubject('');
 
-  constructor() {
-    onAuthStateChanged(this.firebaseAuth, user => {
+  constructor(private notificationService: NotificationHandlingService) {
+    onAuthStateChanged(this.firebaseAuth, (user) => {
       if (user) this.user$.next(user);
-    })
+    });
   }
 
   register(user: UserInterface): Observable<void> {
-    const promise = createUserWithEmailAndPassword(this.firebaseAuth, user.email, user.password).then((response) =>
-      updateProfile(response.user, { displayName: user.username }));
-    return from(promise);
+    return from(
+      createUserWithEmailAndPassword(
+        this.firebaseAuth,
+        user.email,
+        user.password
+      )
+    ).pipe(
+      map((userCredentials: UserCredential) => {
+        sendEmailVerification(userCredentials.user);
+      }),
+      catchError(() => {
+        throw Error;
+      })
+    );
   }
-
 
   login(user: UserInterface): Observable<void> {
-    const promise = signInWithEmailAndPassword(this.firebaseAuth, user.email, user.password)
-      .then(() => {});
+    const promise = signInWithEmailAndPassword(
+      this.firebaseAuth,
+      user.email,
+      user.password
+    ).then(() => {});
     return from(promise);
   }
-
 
   googleSsoLogin(): Observable<void> {
-    const promise = signInWithPopup(this.firebaseAuth, new GoogleAuthProvider())
-      .then(() => { });
+    const promise = signInWithPopup(
+      this.firebaseAuth,
+      new GoogleAuthProvider()
+    ).then(() => {});
     return from(promise);
   }
-
 
   logout(): Observable<void> {
     const promise = signOut(this.firebaseAuth);
     return from(promise);
   }
-  
 
   get currentUser(): Observable<string> {
     return this.user$.asObservable();
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../shared/services/auth.service';
 import { Router } from '@angular/router';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { UserInterface } from '../../shared/modules/user.interface';
 import { Subscription } from 'rxjs';
 import { NotificationHandlingService } from '../../shared/services/notification-handling.service';
@@ -11,10 +17,9 @@ import { NotificationHandlingService } from '../../shared/services/notification-
   standalone: true,
   imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
-
   newUserForm: FormGroup;
   unsubscribeAuth: Subscription = new Subscription();
 
@@ -22,25 +27,38 @@ export class RegisterComponent {
     private authService: AuthService,
     private route: Router,
     private notificationService: NotificationHandlingService
-    ) {
-      this.newUserForm = new FormGroup({
-        username: new FormControl({ value: '', disabled: false }, [Validators.required]),
-        email: new FormControl({ value: '', disabled: false }, [Validators.required, Validators.email]),
-        password: new FormControl({ value: '', disabled: false }, [Validators.required])
-      })
-    }
+  ) {
+    this.newUserForm = new FormGroup({
+      username: new FormControl({ value: '', disabled: false }, [
+        Validators.required,
+      ]),
+      email: new FormControl({ value: '', disabled: false }, [
+        Validators.required,
+        Validators.email,
+      ]),
+      password: new FormControl({ value: '', disabled: false }, [
+        Validators.required,
+      ]),
+    });
+  }
 
-    onSubmit() {
-      const rawUserData = this.newUserForm.getRawValue()
-      const newUser: UserInterface = {username: rawUserData.username, email: rawUserData.email, password: rawUserData.password}
-      this.unsubscribeAuth = this.authService.register(newUser).subscribe({
-        next: () =>this.route.navigateByUrl('verification'),
-        error: (err: Error) => this.notificationService.error(`Something went wrong! ${err.message}`)
-      });
-    }
+  onSubmit() {
+    const rawUserData = this.newUserForm.getRawValue();
+    const newUser: UserInterface = {
+      username: rawUserData.username,
+      email: rawUserData.email,
+      password: rawUserData.password,
+    };
+    this.unsubscribeAuth = this.authService.register(newUser).subscribe({
+      next: () => {
+        this.route.navigateByUrl('verification');
+      },
+      error: (err: Error) =>
+        this.notificationService.error(`Something went wrong! ${err.message}`),
+    });
+  }
 
-
-    ngOnDestroy() {
-      this.unsubscribeAuth.unsubscribe();
-    }
+  ngOnDestroy() {
+    this.unsubscribeAuth.unsubscribe();
+  }
 }
